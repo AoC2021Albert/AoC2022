1b7f85784f5de1eb7ea2d8e232b15b9b99260053
AoC2021Albert <aoc2021albert@protonmail.com>
2022-12-10 11:43:31 +0100

    day 10 finished

--- 10/10.py
+++ 10/10.py
@@ -1,72 +1,38 @@
 #!/usr/bin/env python
-from collections import defaultdict
-from copy import deepcopy
-from functools import cache
-from sys import stdin
-from pprint import pprint
 
 f = open("in.raw", "r")
 lines = f.read().splitlines()
 
+def signal(ops):
+    value = 1
+    for op in ops:
+        if op == "noop":
+            yield(value)
+        else:
+            yield(value)
+            yield(value)
+            value+=int(op.split(" ")[1])
+
+
 # PART 1
-result = 0
-cycle=0
 interesting=[20,60,100,140,180,220]
-#interesting=[1,2,3,4,5,6,7]
-interestingpos=0
-currentval=1
 
-
-def check(interesting,cycle,currentval):
-    global interestingpos
-    global result
-    if interestingpos>=len(interesting):
-        return(0)
-    if cycle >= interesting[interestingpos]:
-        mul=interesting[interestingpos]*currentval
-        print(f'cycle: {cycle}, interestingcycle: {interesting[interestingpos]}, currentval: {currentval}, mul: {mul}')
-        interestingpos+=1
-        return(mul)
-    else:
-        return(0)
-
-for line in lines:
-    if line=="noop":
-        cycle += 1
-        result+=check( interesting,cycle,currentval)
-    else:
-        cycle += 2
-        result+=check( interesting,cycle,currentval)
-        currentval+=int(line.split(" ")[1])
-if interestingpos<len(interesting):
-    result+=currentval
-print(result)
+print(sum(value*pos for pos, value in enumerate(signal(lines),1) if pos in interesting))
 
 # PART 2
-result = 0
-cycle=0
-currentval=1
 HSIZE=40
 VSIZE=6
-screen=[[" "]*HSIZE for _ in range(VSIZE)]
 
-def draw(currentval,screen):
-    global cycle
-    y=(cycle // HSIZE) % VSIZE 
-    x=cycle % HSIZE
-    print(f'x is {x}, currentval is {currentval}')
-    if currentval - 1 <= x <= currentval +1:
-        print(f'painting at {x},{y}')
-        screen[y][x] = "#"
-    cycle+=1
-    
-for line in lines:
-    if line=="noop":
-        draw(currentval,screen)
-    else:
-        draw(currentval,screen)
-        draw(currentval,screen)
-        currentval+=int(line.split(" ")[1])
+screen=[[" "]*HSIZE for _ in range(VSIZE)]
+hpos=0
+vpos=0
+for sprite in signal(lines):
+    if sprite - 1 <= hpos <= sprite + 1:
+        screen[vpos][hpos] = "#"
+    hpos = (hpos + 1) % HSIZE
+    if hpos == 0:
+        vpos = (vpos + 1 ) % VSIZE
+
 for row in screen:
     print("".join(row))
 
