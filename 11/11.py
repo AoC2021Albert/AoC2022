b5a95db918d62a14b7a4425b5dff8aed4f9baec2
AoC2021Albert <aoc2021albert@protonmail.com>
2022-12-11 08:19:25 +0100

    Day 11, cleanup

--- 11/11.py
+++ 11/11.py
@@ -1,99 +1,55 @@
 #!/usr/bin/env python
-from collections import defaultdict
 from copy import deepcopy
-from functools import cache
-from sys import stdin
-from pprint import pprint
 import re
 
-f = open("test.raw", "r")
-
-op = { "+" : lambda a, b : a + b ,
-       "*" : lambda a, b : a * b}
-# PART 1
-ROUNDS=20
-result = 0
-monkeys = []
-while line := f.readline():
-    _ = re.search('Monkey ([0-9]*):',line).group(1)
-    monkey = {}
-    monkey["items"] = [int(item) for item in re.search('Starting items: ([0-9, ]*)$',f.readline()).group(1).split(",")]
-    opval = re.search('Operation: new = old ([+*]) ([0-9old]*)',f.readline())
-    monkey["op"] = opval.group(1)
-    monkey["val"] = opval.group(2)
-    monkey["div"] = int(re.search('Test: divisible by ([0-9]*)',f.readline()).group(1))
-    monkey[True] = int(re.search('If true: throw to monkey ([0-9]*)',f.readline()).group(1))
-    monkey[False] = int(re.search('If false: throw to monkey ([0-9]*)',f.readline()).group(1))
-    print(monkey)
-    monkeys.append(monkey)
-    f.readline()
-
-totalitems=[0] * len(monkeys)
-for round in range(ROUNDS):
-    for i, monkey in enumerate(monkeys):
-        totalitems[i] += len(monkey["items"])
-        for item in monkey["items"]:
-            if monkey["val"] == "old":
-                val = item
-            else:
-                val = int(monkey["val"])    
-            item = op[monkey["op"]](item, val)
-            item = item // 3
-            destmonkey = monkey[item % monkey["div"] == 0]
-            monkeys[destmonkey]["items"].append(item)
-            print(f'pass to {destmonkey} with value {item}')
-        monkey["items"] = []
-    # end of round
-    for i, monkey in enumerate(monkeys):
-        print(f'Monkey {i}: {monkey["items"]}')
-    print(totalitems)
+op = {"+": lambda a, b: a + b,
+      "*": lambda a, b: a * b}
 
 
-totalitems.sort()
-print(totalitems[-1]*totalitems[-2])
+def solver11(ROUNDS, worryreductor, monkeys):
+    totalitems = [0] * len(monkeys)
+    for round in range(ROUNDS):
+        for i, monkey in enumerate(monkeys):
+            totalitems[i] += len(monkey["items"])
+            for item in monkey["items"]:
+                if monkey["val"] == "old":
+                    val = item
+                else:
+                    val = int(monkey["val"])
+                item = op[monkey["op"]](item, val)
+                item = worryreductor(item)
+                destmonkey = monkey[item % monkey["div"] == 0]
+                monkeys[destmonkey]["items"].append(item)
+            monkey["items"] = []
 
-# PART 2
+    totalitems.sort()
+    return (totalitems[-1]*totalitems[-2])
+
+
 f = open("in.raw", "r")
-ROUNDS=10000
-result = 0
+
 monkeys = []
 mul = 1
 while line := f.readline():
-    _ = re.search('Monkey ([0-9]*):',line).group(1)
+    _ = re.search('Monkey ([0-9]*):', line).group(1)
     monkey = {}
-    monkey["items"] = [int(item) for item in re.search('Starting items: ([0-9, ]*)$',f.readline()).group(1).split(",")]
-    opval = re.search('Operation: new = old ([+*]) ([0-9old]*)',f.readline())
+    monkey["items"] = [int(item) for item in re.search(
+        'Starting items: ([0-9, ]*)$', f.readline()).group(1).split(",")]
+    opval = re.search('Operation: new = old ([+*]) ([0-9old]*)', f.readline())
     monkey["op"] = opval.group(1)
     monkey["val"] = opval.group(2)
-    monkey["div"] = int(re.search('Test: divisible by ([0-9]*)',f.readline()).group(1))
+    monkey["div"] = int(
+        re.search('Test: divisible by ([0-9]*)', f.readline()).group(1))
     mul *= monkey["div"]
-    monkey[True] = int(re.search('If true: throw to monkey ([0-9]*)',f.readline()).group(1))
-    monkey[False] = int(re.search('If false: throw to monkey ([0-9]*)',f.readline()).group(1))
-    print(monkey)
+    monkey[True] = int(
+        re.search('If true: throw to monkey ([0-9]*)', f.readline()).group(1))
+    monkey[False] = int(
+        re.search('If false: throw to monkey ([0-9]*)', f.readline()).group(1))
     monkeys.append(monkey)
     f.readline()
 
-totalitems=[0] * len(monkeys)
-for round in range(ROUNDS):
-    for i, monkey in enumerate(monkeys):
-        totalitems[i] += len(monkey["items"])
-        for item in monkey["items"]:
-            if monkey["val"] == "old":
-                val = item
-            else:
-                val = int(monkey["val"])    
-            item = op[monkey["op"]](item, val)
-            item = item % mul
-            destmonkey = monkey[item % monkey["div"] == 0]
-            monkeys[destmonkey]["items"].append(item)
-            print(f'pass to {destmonkey} with value {item}')
-        monkey["items"] = []
-    # end of round
-    for i, monkey in enumerate(monkeys):
-        print(f'Monkey {i}: {monkey["items"]}')
-    print(totalitems)
+# part 1
+print(solver11(20, lambda x: x // 3, deepcopy(monkeys)))
 
-
-totalitems.sort()
-print(totalitems[-1]*totalitems[-2])
-
+# part 2
+print(solver11(10000, lambda x: x % mul, deepcopy(monkeys)))
