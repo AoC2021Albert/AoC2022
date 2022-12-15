deb8db8d45b30d668d2842e49f0c14be932d874c
AoC2021Albert <aoc2021albert@protonmail.com>
2022-12-15 09:25:09 +0100

    Day 15 complete

--- 15/15.py
+++ 15/15.py
@@ -1,39 +1,48 @@
 #!/usr/bin/env python
-from collections import defaultdict
-from copy import deepcopy
-from functools import cache
-from sys import stdin
-from pprint import pprint
 import re
+# LOI = LINE OF INTEREST (part1)
+# MAX = MAX RANGE (part2)
+
+# f = open("test.raw", "r")
+# LOI=10
+# MAX=20
 f = open("in.raw", "r")
-#LOI=10
-#MAX=20
-LOI=2000000
-MAX=4000000
+LOI = 2000000
+MAX = 4000000
 lines = f.read().splitlines()
 
 # PART 1
-segments=[]
+# Segments on the LOI, one per Sensor covering that LOI
+segments = []
+# Beacons present on the LOI.
+# It is a set because the same beacon can appear multiple times in the LOI
 beaconsloi = set()
+
 for line in lines:
     m = re.match("Sensor at x=([0-9-]*), y=([0-9-]*): closest beacon is at x=([0-9-]*), y=([0-9-]*)",
-                line)
-    sx,sy = int(m.group(1)), int(m.group(2))
-    bx,by = int(m.group(3)), int(m.group(4))
-    if by==LOI:
+                 line)
+    # Sesor coordinates
+    sx, sy = int(m.group(1)), int(m.group(2))
+    # Beacon coordinates
+    bx, by = int(m.group(3)), int(m.group(4))
+
+    # Is the beacon in the LOI? add it!
+    if by == LOI:
         beaconsloi.add(bx)
 
-    print(sx,sy,bx,by)
+    # Sensor to Beacon Manhattan Distance, sbmd
     sbmd = abs(sx-bx) + abs(sy-by)
+    # Sensor to LOI Manhattan Distance
     sloimd = abs(sy-LOI)
     if sloimd <= sbmd:
-        dif=sbmd - sloimd
-        print("sensor at ", sx, sy, "create segment : ",[sy-dif, sy+dif] )
+        # This sensor covers the LOI, dif is the "remaining" manhattan distance
+        dif = sbmd - sloimd
         segments.append([sx-dif, sx+dif])
+
+
 segments.sort()
-print(segments)
-result = 0
-curseg=segments[0]
+covered = 0
+curseg = segments[0]
 for segment in segments[1:]:
     if segment[0] <= curseg[1]:
         if segment[1] > curseg[1]:
@@ -43,45 +52,33 @@ for segment in segments[1:]:
             # segment is inside curseg
             ...
     else:
-        #segments do not overlap
-        result+=curseg[1]-curseg[0] + 1
-        curseg=segment
+        # segments do not overlap, count the curseg
+        covered += curseg[1]-curseg[0] + 1
+        curseg = segment
 
-result+=curseg[1]-curseg[0] + 1
-        
+covered += curseg[1]-curseg[0] + 1
 
+print("PART1", covered - len(beaconsloi))
 
-
-print(result, beaconsloi, result- len(beaconsloi))
-print("PART2")
 # PART 2
-segments=[]
-s=[]
-for line in lines:
-    m = re.match("Sensor at x=([0-9-]*), y=([0-9-]*): closest beacon is at x=([0-9-]*), y=([0-9-]*)",
-                line)
-    sx,sy = int(m.group(1)), int(m.group(2))
-    bx,by = int(m.group(3)), int(m.group(4))
-
-    sbmd = abs(sx-bx) + abs(sy-by)
-    s.append((sx,sy,sbmd))
-
 def calcrow(sensors, depth):
-    segments=[]
+    segments = []
     for (sx, sy, sbmd) in sensors:
-        if abs(depth-sy) <=  sbmd:
-            rem = (sbmd-abs(depth-sy))
-            segstart = max(sx-rem,0)
-            segend = min(sx+rem,MAX)
-            #print(depth, sx, sy, sbmd, segstart, segend)
-            if segstart<=segend:
-                segments.append([segstart,segend])
-    #print(segments)
+        if abs(depth-sy) <= sbmd:
+            # This sensor reaches the depth
+            dif = (sbmd-abs(depth-sy))
+            segstart = max(sx-dif, 0)
+            segend = min(sx+dif, MAX)
+            # Once intersected with 0..MAX, do we have a segment left?
+            if segstart <= segend:
+                segments.append([segstart, segend])
     segments.sort()
-    result = 0
-    curseg=segments[0]
+    covered = 0
+    curseg = segments[0]
     if curseg[0] != 0:
-        print("curseg starts at ",curseg[0])
+        # A gap at the beginning of this depth
+        print("Starting gap at:", depth, curseg[0])
+        return(0)
     for segment in segments[1:]:
         if segment[0] <= curseg[1]:
             if segment[1] > curseg[1]:
@@ -91,14 +88,32 @@ def calcrow(sensors, depth):
                 # segment is inside curseg
                 ...
         else:
-            #segments do not overlap (There's a potential gap)
-            print("GAP AT ", depth, curseg, segment, 4000000 * (curseg[1]-1) + depth)
-            result+=curseg[1]-curseg[0] + 1
-            curseg=segment
+            # segments do not overlap (There's a potential gap)
+            if segment[0]-curseg[1] > 1:
+                print("Middle gap at:", depth, curseg, segment)
+                return(curseg[1]+1)
+            covered += curseg[1]-curseg[0] + 1
+            curseg = segment
 
-    result+=curseg[1]-curseg[0] + 1
-    if result != MAX+1:
-        print("depth incomplete at ", depth, result)
+    covered += curseg[1]-curseg[0] + 1
+    if covered != MAX+1:
+        print("Finishing gap at: ", depth, curseg[1])
+        return(4000000)
 
-for i in range(MAX+1):
-    calcrow(s, i)
+    return(None)
+
+
+sensors = []
+for line in lines:
+    m = re.match("Sensor at x=([0-9-]*), y=([0-9-]*): closest beacon is at x=([0-9-]*), y=([0-9-]*)",
+                 line)
+    sx, sy = int(m.group(1)), int(m.group(2))
+    bx, by = int(m.group(3)), int(m.group(4))
+
+    sbmd = abs(sx-bx) + abs(sy-by)
+    # We don't care about beacons anymore, just sensor and SensorBeaconManhattanDistance
+    sensors.append((sx, sy, sbmd))
+
+for depth in range(MAX+1):
+    if gap := calcrow(sensors, depth):
+        print("Part2: ", gap*4000000 + depth)
