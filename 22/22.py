c63d5312c781df13f78adac07bbe32ba91a1f53c
AoC2021Albert <aoc2021albert@protonmail.com>
2022-12-22 09:15:52 +0100

    Day 22 part 2 done

--- 22/22.py
+++ 22/22.py
@@ -6,9 +6,14 @@ from sys import stdin
 from pprint import pprint
 
 f = open("in.raw", "r")
-lines = f.read().splitlines()
 
-# PART 1
+
+RIGHT=0
+DOWN=1
+LEFT=2
+UP=3
+
+lines = f.read().splitlines()
 
 DIRS=((0,1), (1,0), (0, -1), (-1,0))
 
@@ -35,18 +40,57 @@ def bottommost(map,x):
         y-=1
     return(y)
 
+def printmap(map, p, dir):
+    return()
+    y=p[0]
+    x=p[1]
+    for i in range(len(map)):
+        if i==y:
+            print(map[i][:x]+">v<^"[dir]+map[i][x+1:])
+        else:
+            print(map[i])
+    print()
+
+
 def step(map, p, dir):
     y=p[0]
     x=p[1]
+    newdir=dir
     LIMITDETECT = (rightmost(map, y)==x, bottommost(map, x)==y, leftmost(map, y)==x, topmost(map,x)==y )
     if LIMITDETECT[dir]:
-        next = ((y,leftmost(map,y)), (topmost(map,x),x), (y,rightmost(map,y)), (bottommost(map,x),x))[dir]
+
+
+        REMAPRIGHT=(((0),                 (0),                (149-y,99,LEFT)), 
+                    ((0),                 (49,100+(y-50),UP), (0)),
+                    ((0),                 ((149-y),149,LEFT), (0)),
+                    ((149,50+(y-150),UP), (0),                (0)))
+
+        REMAPDOWN = (((0),            (0),                  (50+(x-100),99,LEFT)), 
+                     ((0),            (0),                  (0)),
+                     ((0),            (150+(x-50),49,LEFT), (0)),
+                     ((0,x+100,DOWN), (0),                  (0)))
+
+        REMAPLEFT = (((0), (100+(49-y),0,RIGHT), (0)), 
+                    ((0), (100,y-50,DOWN), (0)),
+                    ((149-y,50,RIGHT), (0), (0)),
+                    ((0,y-150+50,DOWN), (0), (0)))
+
+        REMAPUP = (((0), (x-50+150, 0,RIGHT), (199,x-100, UP)), 
+                ((0), (0), (0)),
+                ((x+50,50,RIGHT), (0), (0)),
+                ((0), (0), (0)))
+
+        ysect=y // 50
+        xsect=x // 50
+        newy, newx, newdir = (REMAPRIGHT,REMAPDOWN,REMAPLEFT,REMAPUP)[dir][ysect][xsect]
+        next =(newy, newx)
+        print(f'next from {p} in direction {("right","down","left","up")[dir]} is {next} in direction {("right","down","left","up")[newdir]}')
     else:
         next = (y+DIRS[dir][0], x+DIRS[dir][1])
     if map[next[0]][next[1]] == ".":
-        return(next)
+        return(next, newdir)
     elif map[next[0]][next[1]] == "#":
-        return((y,x))
+        return((y,x), dir)
     else:
         print("FAIL")
         exit()
@@ -62,9 +106,10 @@ while path[i] in "0123456789":
     i+=1
 dir = 0
 for _ in range(steps):
-    pos=step(map,pos,dir)
+    pos, dir=step(map,pos,dir)
 
 while i<len(path):
+    printmap(map, pos, dir)
     cd = path[i]
     if cd == "R":
         dir += 1
@@ -80,13 +125,9 @@ while i<len(path):
         steps = steps *10 + int(path[i])
         i+=1
     for _ in range(steps):
-        pos=step(map,pos,dir)
+        pos, dir=step(map,pos,dir)
+
+printmap(map, pos, dir)
     
 
 print((pos[0]+1)*1000 + (pos[1]+1) * 4 + dir) 
-
-# PART 2
-result = 0
-for line in lines:
-    ...
-print(result)
