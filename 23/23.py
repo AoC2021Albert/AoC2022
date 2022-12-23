3d1bab410c3f9f6e95f2c8081ac24845f1a3295f
AoC2021Albert <aoc2021albert@protonmail.com>
2022-12-23 08:04:16 +0100

    Day 23

--- 23/23.py
+++ 23/23.py
@@ -1,41 +1,46 @@
 #!/usr/bin/env python
-from collections import defaultdict
-from copy import deepcopy
-from functools import cache
-from sys import stdin
-from pprint import pprint
-
-f = open("test.raw", "r")
-lines = f.read().splitlines()
 
 Y=0
 X=1
-# PART 1
+
 def move(elfs, dirs):
-    newocu = set()
-    collisions = set()
-    newelfs = set()
+    newocu = set()      # New occupied, where elfs propose to go
+    collisions = set()  # Places where more than 1 elf propose to go
+    newelfs = set()     # Where elfs will be after they move (return value)
     AROUND = (
         (-1,-1),(-1,0), (-1,1),
         ( 0,-1),        ( 0,1),
         ( 1,-1),( 1,0), ( 1,1),
     )
+    # Check where elfs propose to go with the goal of detecting collisions
     for elf in elfs:
         d = 0
+        # Iterate the 4 directions and check if movement possible
         while d<4 and any(((elf[Y]+dirs[d][i][Y],elf[X]+dirs[d][i][X]) in elfs for i in range(3))):
+            # Can't move in that direction, try next
             d+=1
+        # if elf does not want to move (AROUND is empty) OR elf can't move (d>=4)
+        # Note: d==0 is just an optimization, do not check AROUND if d>0, as AROUND will be occupied
         if (d==0 and all(((elf[Y]+AROUND[i][Y],elf[X]+AROUND[i][X]) not in elfs for i in range(8))))\
             or d>=4:
+            # elf does not move
             newocu.add(elf)
         else:
+            # elf proposes to move
+            proposed = (elf[Y]+dirs[d][1][Y],elf[X]+dirs[d][1][X])
+            # We could check if proposed is in newocu to detect collisions
+            # But checking if the len of newocu changes is more efficient
             oldlen = len(newocu)
-            newocu.add((elf[Y]+dirs[d][1][Y],elf[X]+dirs[d][1][X]))
+            newocu.add(proposed)
             if oldlen == len(newocu):
-                collisions.add((elf[Y]+dirs[d][1][Y],elf[X]+dirs[d][1][X]))
+                collisions.add(proposed)
+    # Now we move the elfs
     for elf in elfs:
         d = 0
+        # Same as before
         while d<4 and any(((elf[Y]+dirs[d][i][Y],elf[X]+dirs[d][i][X]) in elfs for i in range(3))):
             d+=1
+        # Same as before, but we also check for collisions
         if (d==0 and all(((elf[Y]+AROUND[i][Y],elf[X]+AROUND[i][X]) not in elfs for i in range(8))))\
             or d>=4 \
             or (elf[Y]+dirs[d][1][Y],elf[X]+dirs[d][1][X]) in collisions:
@@ -44,13 +49,8 @@ def move(elfs, dirs):
             newelfs.add((elf[Y]+dirs[d][1][Y],elf[X]+dirs[d][1][X]))
     return(newelfs)
     
-def printmap(elfs):
-    return()
-    map=[["."]*20 for _ in range(20)]
-    for elf in elfs:
-        map[elf[Y]+4][elf[X]+4] = "#"
-    for line in map:
-        print("".join(line))
+f = open("in.raw", "r")
+lines = f.read().splitlines()
 
 elfs=set()
 for y, row in enumerate(lines):
@@ -58,42 +58,33 @@ for y, row in enumerate(lines):
         if cell == "#":
             elfs.add((y,x))
 dirs=[
-    ((-1,-1),(-1, 0),(-1, 1)),
-    (( 1,-1),( 1, 0),( 1, 1)),
-    ((-1,-1),( 0,-1),( 1,-1)),
-    ((-1, 1),( 0, 1),( 1, 1))
+    ((-1,-1),(-1, 0),(-1, 1)), # North
+    (( 1,-1),( 1, 0),( 1, 1)), # South
+    ((-1,-1),( 0,-1),( 1,-1)), # West
+    ((-1, 1),( 0, 1),( 1, 1))  # East
 ]
 
-printmap(elfs)
-pprint(dirs)
-for i in range(100000):
+def emptyspaces(elfs):
+    maxx=-999999999999
+    maxy=-999999999999
+    minx=999999999999
+    miny=999999999999
+
+    for elf in elfs:
+        minx=min(minx,elf[X])
+        maxx=max(maxx,elf[X])
+        miny=min(miny,elf[Y])
+        maxy=max(maxy,elf[Y])
+    return((maxx-minx+1)*(maxy-miny+1) - len(elfs))
+
+iteration=0
+while True:
+    if iteration==10:
+        print("Part 1: ", emptyspaces(elfs))
+    iteration += 1
     newelfs = move(elfs, dirs)
     if len(newelfs.difference(elfs))==0:
-        print(i)
+        print("Part 2: ", iteration)
         exit()
-    #printmap(elfs)
     dirs=dirs[1:]+dirs[:1]
-    #pprint(dirs)
     elfs=newelfs
-
-
-maxx=-999999999999
-maxy=-999999999999
-minx=999999999999
-miny=999999999999
-
-for elf in elfs:
-    minx=min(minx,elf[X])
-    maxx=max(maxx,elf[X])
-    miny=min(miny,elf[Y])
-    maxy=max(maxy,elf[Y])
-
-print(maxx,minx,maxy,miny)
-
-print((maxx-minx+1)*(maxy-miny+1) - len(elfs))
-
-# PART 2
-result = 0
-for line in lines:
-    ...
-print(result)
