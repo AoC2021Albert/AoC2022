c24d5ecf68eed9aace19fa75a02682217f54d4a8
AoC2021Albert <aoc2021albert@protonmail.com>
2022-12-23 13:49:27 +0100

    Day 20

--- 20/20.py
+++ 20/20.py
@@ -5,6 +5,7 @@ from functools import cache
 from sys import stdin
 from pprint import pprint
 
+
 class Dll:
     def __init__(self, value):
         self.prev = None
@@ -12,74 +13,73 @@ class Dll:
         self.next = None
 
     def __repr__(self):
-        return(f'{self.prev.value}<{self.value}>{self.next.value}')
+        return (f'{self.prev.value}<{self.value}>{self.next.value}')
 
-    def moveforward(self, n):
-        for _ in range(n * 811589153 % LENL):
-            mynext = self.next
-            myprev = self.prev
-            mynextnext = self.next.next
-            myprev.next = mynext
-            self.prev = mynext
-            mynext.prev = myprev
-            self.next = mynextnext
-            mynextnext.prev = self
-            mynext.next = self
-            
-    def movebackward(self, n):
-        for _ in range(n * 811589153 % LENL ):
-            mynext = self.next
-            myprev = self.prev
-            myprevprev = self.prev.prev
-            mynext.prev = myprev
-            self.next = myprev
-            myprev.next = mynext
-            self.prev = myprevprev
-            myprevprev.next = self
-            myprev.prev = self
-            
+    def move(self, n, multiplier, modulo, forward):
+        oldnext = self.next
+        oldprev = self.prev
+        if forward:
+            newprev = self
+        else:
+            newprev = self.prev
+        for _ in range(n * multiplier % modulo):
+            if forward:
+                newprev = newprev.next
+            else:
+                newprev = newprev.prev
+        newnext = newprev.next
+
+        oldprev.next, oldnext.prev = oldnext, oldprev
+
+        newprev.next = self
+        newnext.prev = self
+        self.prev = newprev
+        self.next = newnext
 
 
 f = open("in.raw", "r")
 lines = f.read().splitlines()
 
-# PART 2
-result = 0
-l = []
-v0 = None
-LENL = len(lines) -1
-for line in lines:
-    value = int(line)
-    v = Dll(value)
-    l.append(v)
-    if value==0: 
-        v0 = v
 
-for i in range(len(l)):
-    l[i].prev=l[(i-1) % len(l)]
-    l[i].next=l[(i+1 )% len(l)]
+def solve20(lines, multiplier, rounds):
+    # Initialize and load
+    result = 0
+    l = []
+    v0 = None
+    LENL = len(lines) - 1
+    for line in lines:
+        value = int(line)
+        v = Dll(value)
+        l.append(v)
+        if value == 0:
+            v0 = v
 
-for _ in range(10):
-    for v in l:
-        if v.value > 0 :
-            v.moveforward(v.value)
-        if v.value < 0 :
-            v.movebackward(-v.value)
+    for i in range(len(l)):
+        l[i].prev = l[(i-1) % len(l)]
+        l[i].next = l[(i+1) % len(l)]
 
-newl = [0]
-curv = v0.next
-while curv.value != 0:
-    newl.append(curv.value)
-    curv = curv.next
+    # Do rounds
+    for _ in range(rounds):
+        for v in l:
+            if v.value > 0:
+                v.move(v.value, multiplier, LENL, True)
+            if v.value < 0:
+                v.move(-v.value, multiplier, LENL, False)
 
-for k in 1000, 2000, 3000:
-    num = newl[k % len(newl)]
-    print(num * 811589153)
-    result+=num* 811589153
-print(result)
+    # Get solution
+    # Convert dll to list
+    newl = [0]
+    curv = v0.next
+    while curv.value != 0:
+        newl.append(curv.value)
+        curv = curv.next
 
-# PART 2
-result = 0
-for line in lines:
-    ...
-print(result)
+    # Calculate result
+    for k in 1000, 2000, 3000:
+        num = newl[k % len(newl)]
+        result += num * multiplier
+    return(result)
+
+
+print("Part1: ", solve20(lines, 1, 1))
+print("Part2: ", solve20(lines, 811589153, 10))
